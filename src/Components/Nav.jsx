3447c02b5482dfbea47ec9c13c2fd3ddb5b17199
thebhaveshshinde23 <thebhaveshshinde@gmail.com>
2024-11-08 11:02:51 +0530

    Cancel nav button Add

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -16,13 +16,16 @@ function Nav() {
     <>
       <nav className="navbar">
         
-        <ul className={`nav-links ${isopen?'open' : ''}`}>
+        <ul className={`nav-links ${isopen?'open' : ''} `}>
             <NavLink onClick={handle} activeclassname="active" to={"/"}>Home</NavLink>
             <NavLink onClick={handle} activeclassname="active" to={"/about"}>About</NavLink>
             <NavLink onClick={handle} activeclassname="active" to={"/blogs"}>Blogs</NavLink>
         </ul>
         <div className="hambergur_logo_container">
-             <img src="/download2.png" alt="Hambergur Logo Pic " onClick={handle_logo_click} />
+             <img className={` img ${isopen?'img_isopen' :''}`}  src="/download2.png" alt="Hambergur Logo Pic " onClick={handle_logo_click} />
+        </div>
+        <div className="cross_logo_container">
+          <img className={`img2 ${isopen?'cross_show':''}`} onClick={handle} src="/download3.png" alt="cross Logo" />
         </div>
       </nav>
     </>
